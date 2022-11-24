9f749ce42889a506b2bd9a045daea6e09089f58b
Nofalll <115127476+Nofalll@users.noreply.github.com>
2022-11-24 12:42:54 +0700

    Create variabel.java

---
+++ variabel.java
@@ -0,0 +1,13 @@
+public class MyClass {
+    public static void main(String[] args) {
+        String salam = "Assalamualaikum";
+        String nama = "Muhamad Noufal falah";
+        int tahun = 2004;
+        
+        System.out.println(salam);
+        System.out.println("Nama Saya "+nama);
+        System.out.println("Saya Lahir Di Bogor Tahun : "+tahun);
+    
+
+    }
+}
